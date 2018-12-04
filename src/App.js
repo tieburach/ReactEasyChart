85bd67acfacf786fa003d5ca7f434a4bd8637c28
Tieburach <witalis-kinasz@wp.pl>
2018-12-04 18:53:26 +0100

    "testing"

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
                 <header className={"App-header"}>
                     <img src={logo} className={"App-logo"} alt={"logo"}/>
                     <p>
-                        To bedzie nasz projekcik.
+                        To bedzie nasz projekcik z reacta.
                     </p>
                 </header>
                 <div className={"App-menu"}>
