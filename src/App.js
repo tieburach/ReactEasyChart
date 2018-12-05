818679a28b29994f082e4dc41c75f3e103fb9fcb
Kamil <kamil.makarewicz@raiffeisen.pl>
2018-12-05 18:37:44 +0100

    CSV upload

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, {Component} from 'react';
 import logo from './logo.jpg';
+import { CSVLink } from "react-csv";
 import './App.css';
+import ReactDOM from 'react-dom'
+import CSVReader from 'react-csv-reader'
 
 class App extends Component {
+
     render() {
+        var csvData = [];
         return (
             <div className={"App"}>
                 <header className={"App-header"}>
@@ -14,8 +19,21 @@ class App extends Component {
                 </header>
                 <div className={"App-menu"}>
                     <p>Napis w menu</p>
+                    <CSVReader
+                        id={"csvka"}
+                        cssClass="csv-input"
+                        label="Elo mordo"
+                        onFileLoaded={csvData.push([this.props])}
+                        onError={this.handleDarkSideForce}
+                        inputId="ObiWan"
+                        inputStyle={{color: 'red'}}
+                    />
+                    <CSVLink data={csvData}
+                             separator={";"}
+                             filename={"my-file.csv"}>
+                        Download me
+                    </CSVLink>;
                 </div>
-
                 <div className="Main-div">
                     <p>Napis w main</p>
                 </div>
