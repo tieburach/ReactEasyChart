159090dfe5326e49ebe019d812359ab0e9ab7bdc
Tieburach <witalis-kinasz@wp.pl>
2018-12-15 15:14:27 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
 import React, {Component} from 'react';
-import logo from './resources/logo.PNG';
-import Menu from "./menu/Menu";
 import './css/App.css';
+import Chart from './components/Chart';
 import CSVManager from "./CSVManager";
-
+import Menu from "./menu/Menu";
+import logo from "./resources/logo.PNG"
 
 class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            chartData: {}
+        }
+    }
+
+    componentWillMount() {
+        this.getChartData();
+    }
+
+    getChartData() {
+        // Ajax calls here
+        this.setState({
+            chartData: {
+                labels: ['Boston', 'Worcester', 'Springfield', 'Lowell', 'Cambridge', 'New Bedford'],
+                datasets: [
+                    {
+                        label: 'Population',
+                        data: [
+                            617594,
+                            181045,
+                            153060,
+                            106519,
+                            105162,
+                            95072
+                        ],
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.6)',
+                            'rgba(54, 162, 235, 0.6)',
+                            'rgba(255, 206, 86, 0.6)',
+                            'rgba(75, 192, 192, 0.6)',
+                            'rgba(153, 102, 255, 0.6)',
+                            'rgba(255, 159, 64, 0.6)',
+                            'rgba(255, 99, 132, 0.6)'
+                        ]
+                    }
+                ]
+            }
+        });
+    }
 
     render() {
         return (
@@ -17,11 +58,11 @@ class App extends Component {
                     <Menu/>
                 </div>
                 <div className="Main-div">
-                    <CSVManager/>
+                    <Chart chartData={this.state.chartData} location="Massachusetts" legendPosition="bottom"/>
                 </div>
+                <CSVManager/>
             </div>
         );
     }
 }
-
 export default App;
--- src/CSVManager.js
+++ src/CSVManager.js
@@ -1,22 +1,22 @@
 import React, {Component} from "react";
 import {CSVLink} from "react-csv";
 import CSVReader from 'react-csv-reader'
-var csvData = [];
 class CSVManager extends Component {
 
     render() {
+        var data=[];
         return (
             <div className="Main-div">
                 <CSVReader
                     id={"csvka"}
                     cssClass="csv-input"
                     label="Elo mordo"
-                    onFileLoaded={csvData.push([this.props])}
+                    onFileLoaded={data.push([this.props])}
                     onError={this.handleDarkSideForce}
                     inputId="ObiWan"
                     inputStyle={{color: 'red'}}
                 />
-                <CSVLink data={csvData}
+                <CSVLink data={data}
                          separator={";"}
                          filename={"my-file.csv"}>
                     Download me
