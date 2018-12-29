1dd3fcedf20670dca83e402ed9a54ee352a9f947
Tieburach <witalis-kinasz@wp.pl>
2018-12-29 12:19:15 +0100

    zmiany

--- src/App.js
+++ src/App.js
@@ -5,96 +5,10 @@ import CSVManager from "./components/CSVManager";
 import Menu from "./components/Menu";
 import logo from "./resources/logo.PNG"
 
-class Przycisk extends React.Component {
-    constructor(props) {
-        super(props);
-        this.onPClicked = props.onPClicked;
-    }
-
-    onPrzyciskClicked(e) {
-        console.log("Wcisnalem." + this);
-        this.onPClicked(e);
-    }
-
-    render() {
-        return (
-            <button onClick={e => this.onPrzyciskClicked(e)}>Wciśnij mnie</button>
-        );
-    }
-}
-
 class App extends Component {
     constructor() {
         super()
-        this.state = this.getChartData();
     }
-
-    componentWillMount(){
-        this.getChartData();
-
-    }
-
-    getChartData() {
-        this.setState({
-            chartData: {
-                labels: ['Boston', 'Worcester', 'Springfield', 'Lowell', 'Cambridge', 'New Bedford'],
-                datasets: [
-                    {
-                        label: 'Population',
-                        data: [
-                            617594,
-                            181045,
-                            153060,
-                            106519,
-                            105162,
-                            95072
-                        ],
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.6)',
-                            'rgba(54, 162, 235, 0.6)',
-                            'rgba(255, 206, 86, 0.6)',
-                            'rgba(75, 192, 192, 0.6)',
-                            'rgba(153, 102, 255, 0.6)',
-                            'rgba(255, 159, 64, 0.6)',
-                            'rgba(255, 99, 132, 0.6)'
-                        ]
-                    }
-                ]
-            }
-        });
-    }
-
-    onClick = () => {
-        this.setState({
-            chartData: {
-                labels: ['Boston', 'Worcester', 'Springfield', 'Lowell', 'Cambridge', 'New Bedford'],
-                datasets: [
-                    {
-                        label: 'Population',
-                        data: [
-                            200,
-                            200,
-                            200,
-                            200,
-                            200,
-                            200
-                        ],
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.6)',
-                            'rgba(54, 162, 235, 0.6)',
-                            'rgba(255, 206, 86, 0.6)',
-                            'rgba(75, 192, 192, 0.6)',
-                            'rgba(153, 102, 255, 0.6)',
-                            'rgba(255, 159, 64, 0.6)',
-                            'rgba(255, 99, 132, 0.6)'
-                        ]
-                    }
-                ]
-            }
-        });
-    }
-
-
     render() {
         return (
             <div className={"App"}>
@@ -103,12 +17,10 @@ class App extends Component {
                 </header>
                 <div className={"App-menu"}>
                     <Menu/>
-                    <Przycisk onPClicked={(e) => this.onClick(e)} />
                 </div>
                 <div className="Main-div" >
-                    <Chart chartData={this.state.chartData}  legendPosition="bottom"/>
+                    <Chart/>
                 </div>
-                <CSVManager/>
             </div>
         );
     }
--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,41 +1,83 @@
 import React, {Component} from 'react';
-import {Bar, Line, Pie} from 'react-chartjs-2';
+import HighchartsReact from "highcharts-react-official";
+import Highcharts from "highcharts";
+
 
 class Chart extends Component{
   constructor(props){
     super(props);
-    this.state = {
-      chartData:props.chartData
-    }
   }
 
-  static defaultProps = {
-    displayTitle:true,
-    displayLegend: true,
-    legendPosition:'right',
-    location:'City'
-  }
+    onClick = () => {
+        console.log(this.refs.highCharts);
+        if (this.refs.highCharts) {
+            this.refs.highCharts.chart.series[0].update({
+                type: "line"
+            });
+            this.refs.highCharts.chart.series[1].update({
+                type: "line"
+            });
+            this.refs.highCharts.chart.series[2].update({
+                type: "line"
+            });
+        } else {
+            console.log("not working");
+        }
+    };
 
-  render(){
-    return (
-      <div className="chart">
-        <Bar
-          data={this.state.chartData}
-          options={{
-            title:{
-              display:this.props.displayTitle,
-              text:'Largest Cities In '+this.props.location,
-              fontSize:25
+    onClickBar = () => {
+        this.refs.highCharts.chart.series[0].update({
+            type: "bar"
+        });
+        this.refs.highCharts.chart.series[1].update({
+            type: "bar"
+        });
+        this.refs.highCharts.chart.series[2].update({
+            type: "bar"
+        });
+    };
+
+
+    render() {
+        const options = {
+            title: {
+                text: "Test"
             },
-            legend:{
-              display:this.props.displayLegend,
-              position:this.props.legendPosition
-            }
-          }}
-        />
-      </div>
-    )
-  }
+            xAxis: {
+                categories: ["Apples", "Oranges", "Pears", "Grapes", "Bananas"]
+            },
+            credits: {
+                enabled: false
+            },
+            series: [
+                {
+                    name: "John",
+                    data: [5, 3, 4, 7, 2],
+                    type: "column"
+                },
+                {
+                    name: "Jane",
+                    data: [2, -2, -3, 2, 1],
+                    type: "column"
+                },
+                {
+                    name: "Joe",
+                    data: [3, 4, 4, -2, 5],
+                    type: "column"
+                }
+            ]
+        };
+        return (
+            <div>
+                <HighchartsReact
+                    ref="highCharts"
+                    highcharts={Highcharts}
+                    options={options}
+                />
+                <button onClick={this.onClick}>Line</button>
+                <button onClick={this.onClickBar}>Bar</button>
+            </div>
+        );
+    }
 }
-
 export default Chart;
