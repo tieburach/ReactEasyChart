0aefb02c18cab16ae963359eecceff3b4a969259
Tieburach <witalis-kinasz@wp.pl>
2019-01-09 11:09:58 +0100

    "poprawki na datepicker"

--- src/components/DataPicker.js
+++ src/components/DataPicker.js
@@ -1,59 +1,114 @@
 import React from "react";
 import ReactDataGrid from "react-data-grid";
-import Data from "../Data";
 
-const columns = [
-    { key: "first", name: "first", editable: true },
-    { key: "second", name: "second", editable: true },
-    { key: "third", name: "third", editable: true }
-];
 
 const rows = [
-    { first: 0, second: 1, third: 2 },
-    { first: 1, second: 2, third: 4 },
-    { first: 2, second: 3, third: 6 }
+    { 1: 0, 2: 1, 3: 2 },
+    { 1: 1, 2: 2, 3: 4 },
+    { 1: 2, 2: 3, 3: 6 }
 ];
+
+const columns = [
+    {
+        key: 1,
+        name: 1,
+        editable:true
+    },
+    {
+        key: 2,
+        name: 2,
+        editable: true
+    },
+    {
+        key: 3,
+        name: 3,
+        editable: true
+    },
+    {
+        key: 4,
+        name: 4,
+        editable: true
+    }
+]
+
 
 export default class DataPicker extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.rows = rows;
+        this.state = {
+            rows : rows,
+            columns : columns
+        };
+        this.rowGetter = this.rowGetter.bind(this);
+        this.handleGridRowsUpdated = this.handleGridRowsUpdated.bind(this);
+        this.addNewRow = this.addNewRow.bind(this);
+        this.addNewColumn = this.addNewColumn.bind(this);
     }
 
-    onGridRowsUpdated = ({ fromRow, toRow, updated }) => {
-            this.rows.slice();
+    rowGetter(i) {
+        return this.state.rows[i];
+    }
+
+    handleGridRowsUpdated = ({ fromRow, toRow, updated }) => {
+        this.setState(state => {
+            const rows = state.rows.slice();
             for (let i = fromRow; i <= toRow; i++) {
                 rows[i] = { ...rows[i], ...updated };
             }
-            this.setState(this.rows = rows);
-            let first =[];
-            let second =[];
-            let third=[];
-            for (let i =0; i<this.rows.length; i++ ){
-                first[i] = parseInt("" +this.rows[i].first);
-                second[i] = parseInt("" +this.rows[i].second);
-                third[i] = parseInt("" +this.rows[i].third)
-            }
-
-            console.log(first)
-
-            Data.dataSeries[0].data = first;
-            Data.dataSeries[1].data = second;
-            Data.dataSeries[2].data = third;
-            this.props.reloadDataSeries();
-
+            return { rows };
+        });
     };
 
-    render() {
-        return (
-            <ReactDataGrid
-                columns={columns}
-                rowGetter={i => rows[i]}
-                rowsCount={3}
-                onGridRowsUpdated={this.onGridRowsUpdated}
-                enableCellSelect={true}
-            />
-        );
+    addNewRow(ev) {
+        ev.preventDefault();
+        let rows = this.state.rows;
+
+        rows.push({
+            id : this.state.rows.length,
+            recipient_name : '',
+            recipient_email : '',
+            subject_parameters : '',
+            body_parameters : '',
+            attachments : '',
+        });
+        this.setState({ rows : rows });
     }
+
+    addNewColumn(ev) {
+        console.log(this.state.columns);
+        ev.preventDefault();
+        let columns = this.state.columns;
+
+        columns.push({
+            key: this.state.rows.length + 1,
+            name: this.state.rows.length + 1,
+            editable: true
+        });
+        this.setState({ columns : columns });
+        console.log(this.state.columns);
+    }
+
+
+    render() {
+        return (<div className="emailRecipientsEditor">
+
+            <div className="emailRecipientsEditor__newButton">
+                <button onClick={this.addNewRow} className="btn btn-default">Add row</button>
+                <button onClick={this.addNewColumn} className="btn btn-default">Add column</button>
+            </div>
+
+            <ReactDataGrid
+                enableCellSelect={true}
+                columns={this.state.columns}
+                rowGetter={this.rowGetter}
+                rowsCount={this.state.rows.length}
+                minHeight={400}
+                onGridRowsUpdated={this.handleGridRowsUpdated}
+            />
+
+        </div>);
+    }
+
+
 }
